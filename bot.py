20a5d1a0c7d1cb4c854ef43d9a8ca54ba0a61979
dnulyb <henrik.bylund@proton.me>
2024-01-23 13:38:58 +0100

    Tasks working. Startup function added.

--- bot.py
+++ bot.py
@@ -1,20 +1,10 @@
 import pkgutil
 
 import interactions
-from interactions import listen, Task, IntervalTrigger
-from interactions.api.events import Startup
 from dotenv import find_dotenv, load_dotenv, get_key
 
-import src.db.db as db
-from src.ubi.authentication import *
 
-from src.ubi.authentication import check_token_refresh
-from src.rss import check_for_new_tweets
-import asyncio
-
-
-
-# load bot variables
+# Load bot variables
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 TOKEN = get_key(dotenv_path, "DISCORD_TOKEN")
@@ -25,44 +15,11 @@ bot = interactions.Client(
     # set debug_scope to not be in global scope
     debug_scope=GUILD_ID, 
 )
-#bot.interaction_tree.update()
-
-# set up db
-db.init()
 
 # Load all extensions
 extensions = [m.name for m in pkgutil.iter_modules(["src/commands"], prefix="src.commands.")]
 for extension in extensions:
     bot.load_extension(extension)
 
-# Tasks
-@Task.create(IntervalTrigger(minutes=30))
-async def update_nadeo_token():
-    return
-    print("Checking if Nadeo access token needs an update...")
-    check_token_refresh()
-
-@Task.create(IntervalTrigger(minutes=1))
-async def check_tweets(self, ctx):
-
-    #await ctx.send("I'm supposed to check for new tweets, but it's disabled atm.")
-    print("I'm supposed to check for new tweets, but it's disabled atm.")
-    return
-
-    print("Checking for new tweets...")
-    tweets = check_for_new_tweets()
-
-    for tweet in tweets:
-
-        await ctx.send(tweet)
-
-        # It's best to have at least some delay between posting new tweets
-        asyncio.sleep(10)
-
-@listen(Startup)
-async def on_startup():
-    update_nadeo_token.start()
-    check_tweets.start()
-
-
+# Start the bot
 bot.start(TOKEN)
--- src/commands/bot_management.py
+++ src/commands/bot_management.py
@@ -3,10 +3,21 @@ from interactions import (
     slash_command, 
     SlashContext,
     check, 
-    is_owner
+    is_owner,
+    Client,
+    listen,
+    Task,
+    IntervalTrigger
 )
+from interactions.api.events import Startup
 
 import pkgutil
+import asyncio
+from dotenv import find_dotenv, load_dotenv, get_key
+
+import src.db.db as db
+from src.ubi.authentication import check_token_refresh
+from src.rss import check_for_new_tweets
 
 """
 #custom check example
@@ -49,3 +60,51 @@ class BotManagement(Extension):
     )
     async def ping(self, ctx: SlashContext):
         await ctx.send("pong")
+
+    # Tasks
+    @Task.create(IntervalTrigger(minutes=30))
+    async def update_nadeo_token(self):
+        print("Checking if Nadeo access token needs an update...")
+        check_token_refresh()
+
+    @Task.create(IntervalTrigger(minutes=30))
+    async def check_tweets(self, bot: Client):
+
+        dotenv_path = find_dotenv()
+        load_dotenv(dotenv_path)
+
+        channel_id = get_key(dotenv_path, ("DISCORD_TWITTER_CHANNEL"))
+        channel = bot.get_channel(channel_id)
+
+        print("Checking for new tweets...")
+        tweets = check_for_new_tweets()
+
+        if(len(tweets) == 0):
+            # No new tweets
+            print("No new tweets found.")
+            return
+        else:
+            print(len(tweets), " tweets found! Posting...")
+
+        for tweet in tweets:
+
+            await channel.send(tweet)
+
+            # It's best to have at least some delay between posting new tweets
+            await asyncio.sleep(10)
+
+        print("Finished checking for new tweets.")
+
+    @listen(Startup)
+    async def on_startup(self, event: Startup):
+
+        # Make sure db is set up
+        db.init()
+
+        # Initial checks
+        await self.update_nadeo_token()
+        await self.check_tweets(event.client)
+
+        # Start tasks
+        self.update_nadeo_token.start()
+        self.check_tweets.start(event.client)
